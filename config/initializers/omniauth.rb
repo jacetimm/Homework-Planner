9b4263e89fd71e587fa8705b2565018bd75c8a8b
Jace Timm <jacetimm@gmail.com>
2026-03-26 19:31:31 -0400

    remove drive.readonly scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
            Rails.application.credentials.dig(:google, :client_id),
            Rails.application.credentials.dig(:google, :client_secret), {
-    scope: "email,profile,https://www.googleapis.com/auth/classroom.courses.readonly,https://www.googleapis.com/auth/classroom.coursework.me.readonly,https://www.googleapis.com/auth/classroom.student-submissions.me.readonly,https://www.googleapis.com/auth/calendar.readonly,https://www.googleapis.com/auth/drive.readonly",
+    scope: "email,profile,https://www.googleapis.com/auth/classroom.courses.readonly,https://www.googleapis.com/auth/classroom.coursework.me.readonly,https://www.googleapis.com/auth/classroom.student-submissions.me.readonly,https://www.googleapis.com/auth/calendar.readonly",
     # "consent" forces the full OAuth flow without the prompt=none edge case
     # that causes the session state to be lost and bytesize to fail on nil
     prompt: "consent",
